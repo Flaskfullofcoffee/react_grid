df5c40728e89ed675c86d46fcb43718f4c14e63f
Zac <zdobbs3@gmail.com>
2019-05-30 06:08:14 -0700

    added content containers

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react'
-import TodoItem from './Todo.js'
-import todosData from "./todosData"
 import './App.css'
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-    handleChange(id) {
-      this.setState(prevState => {
-        const updatedTodos = prevState.todos.map(todo => {
-          if (todo.id === id) {
-            todo.completed = !todo.completed;
-          }
-          return todo
-        })
-        return {
-          todos: updatedTodos
-        }
-      })
-    }
 
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-
-        return (
-            <div className="todo-list">
-                {todoItems}
+      return (
+            <div>
+              <Carousel />
+              <Games />
+              <Rates />
+              <Contact />
+              <Location />
             </div>
         )
     }
