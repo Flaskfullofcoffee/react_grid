ffaa9e396d16d291c26b6985df44f33102249457
Zac <zdobbs3@gmail.com>
2019-05-30 06:11:55 -0700

    connected containers to App.js

---
+++ src/Games.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class Games extends Component {
+
+    render() {
+      return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Games
