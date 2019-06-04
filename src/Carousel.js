c99e8912865ec1e48bbe68babea364d53535d834
Zac <zdobbs3@gmail.com>
2019-06-04 10:39:54 -0700

    set up counter for carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react'
 import './App.scss'
 
 class Carousel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.timer = this.timer.bind(this)
+  }
+  componentDidMount() {
+    let intervalId = setInterval(this.timer, 6000);
+    this.setState({ intervalId: intervalId });
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  timer() {
+    this.state.count >= 2 ? this.setState({ count: 0 }) : this.setState({ count: this.state.count +1 })
+  }
+
 
     render() {
+
       return (
             <section className='carousel'>
               <div className='slider'>
-                <div className='slide'></div>
-                <div className='slide'></div>
-                <div className='slide'></div>
+                {this.state.count}
               </div>
               <div className='dots-wrapper'></div>
             </section>
