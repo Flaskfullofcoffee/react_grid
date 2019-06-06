08a02bc4a14130287b648ba8f712fab629fe6c35
Zac <zdobbs3@gmail.com>
2019-06-06 15:13:45 -0700

    set framework for rates & prices sections

---
+++ src/PriceInfo.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+class PriceInfo extends Component {
+
+    render() {
+      return (
+          <section className='price-info'>
+            <div className='price-wrapper'>
+              <div className='card'>
+                <h3>Standard</h3>
+                <p className='cost'>$5/hr</p>
+                <hr />
+                <p>Intel i5 7<sup>th</sup> Gen</p>
+                <hr />
+                <p>16GB Ram</p>
+                <hr />
+                <p>NVIDIA GTX 1070</p>
+                <hr />
+                <p>BenQ 24" esports 240hz xl's</p>
+                <hr />
+              </div>
+              <div className='card'>
+                <h3>VIP</h3>
+                <p className='cost'>$7/hr</p>
+                <hr />
+                <p>Intel i7 7<sup>th</sup> Gen</p>
+                <hr />
+                <p>16GB Ram</p>
+                <hr />
+                <p>NVIDIA GTX 1080</p>
+                <hr />
+                <p>BenQ 27" 2K esports 144hz xl's</p>
+                <hr />
+              </div>
+            </div>
+          </section>
+        )
+    }
+}
+
+export default PriceInfo
--- src/Rates.js
+++ src/Rates.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PriceInfo from './PriceInfo.js'
 import './App.scss'
 
 class Rates extends Component {
@@ -6,7 +7,18 @@ class Rates extends Component {
     render() {
       return (
           <section className='rates'>
-
+            <h2 className='title'>Rates</h2>
+            <div className='location-rate'>
+              <button className='btn tempe'>Tempe</button>
+              <button className='btn phoenix'>Phoenix</button>
+            </div>
+            <div className='prices'>
+              <button className='btn hourly'>Hourly</button>
+              <button className='btn binge'>Binge</button>
+              <button className='btn bulk'>Bulk</button>
+              <button className='btn twenty-four'>24hr Binge</button>
+            </div>
+            <PriceInfo />
           </section>
         )
     }
