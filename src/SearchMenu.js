0256377188988675b9ed8bad1844de0b651210ec
Zac <zdobbs3@gmail.com>
2020-01-07 13:28:38 -0700

    update git

--- src/SearchMenu.js
+++ src/SearchMenu.js
@@ -4,7 +4,9 @@ import './App.scss'
 class SearchMenu extends Component {
 
   componentDidMount() {
-    const url = "https://script.googleusercontent.com/macros/echo?user_content_key=khx_184a-zMF7xYEFiYwu3Rg3aqOkiyXe-hx2GLtAf2CDBI_Z_C8VPhTxcblTLeQyLt0KCPT-DP30wsV7HrgwHlptyN6pCGom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKM1ySAP19am3HiAMd7ZlOlZXAaDBQajCVFOzrP3sb9pspWv2o1lwZigsAXUwlIHhsTvy1m0tLyP&lib=M2cybmF92Md50tz0Q_PGZ20xzvpP3hh6O"
+    const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSAQwfd-wwVm7NraaaTdMgIzVwjmZDZyVAO1EyUbNzoFeyse16ftOS1tkUe0-w-EY14erzSszZBTh5J/pubhtml"
+
+    // "https://script.googleusercontent.com/macros/echo?user_content_key=khx_184a-zMF7xYEFiYwu3Rg3aqOkiyXe-hx2GLtAf2CDBI_Z_C8VPhTxcblTLeQyLt0KCPT-DP30wsV7HrgwHlptyN6pCGom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKM1ySAP19am3HiAMd7ZlOlZXAaDBQajCVFOzrP3sb9pspWv2o1lwZigsAXUwlIHhsTvy1m0tLyP&lib=M2cybmF92Md50tz0Q_PGZ20xzvpP3hh6O"
 
     fetch(url)
       .then((resp) => resp.json())
